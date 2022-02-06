54f812c03a33aeb10ecebdae72e5b3d488cb006e
baconQ <omar226109@gmail.com>
2022-02-06 15:53:17 +0300

    add food

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from snake import Snake
 from turtle import Screen
+from food import Food
 
 screen = Screen()
 screen.setup(width=800, height=800)
@@ -8,15 +9,20 @@ screen.title("Slither Sam")
 screen.tracer(0)
 screen.listen()
 sam = Snake()
+food = Food()
+screen.onkey(sam.left, "a")
+screen.onkey(sam.right, "d")
+screen.onkey(sam.up, "w")
+screen.onkey(sam.down, "s")
+
 
 is_snake_alive = True
 
 while is_snake_alive:
     sam.move()
-    screen.onkey(sam.left, "a")
-    screen.onkey(sam.right, "d")
-    screen.onkey(sam.up, "w")
-    screen.onkey(sam.down, "s")
+    
+    if sam.head.distance(food) < 15:
+        food.refresh()
     
 
 screen.exitonclick()
